1653f80b45103906d1ce252eabb5946fad6f08cb
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-07-05 19:51:30 -0300

    Initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- move 1/2 screen up/down and center vertically
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
