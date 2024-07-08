3a96cec2c543147abae73e1f97eedde5d6fba2f7
brandon bailey <brandonbailey@brandons-mbp.home>
2024-07-08 11:39:23 -0300

    fix unit test configuration for monorepo

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -6,10 +6,19 @@ return {
       table.insert(
         opts.adapters,
         require("neotest-jest")({
-          jestCommand = "npm test --",
-          jestConfigFile = "custom.jest.config.ts",
+          jestCommand = "npx jest",
+          jestConfigFile = function(file)
+            if string.find(file, "/packages/") then
+              return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+            end
+
+            return vim.fn.getcwd() .. "/jest.config.ts"
+          end,
           env = { CI = true },
-          cwd = function()
+          cwd = function(file)
+            if string.find(file, "/packages/") then
+              return string.match(file, "(.-/[^/]+/)src")
+            end
             return vim.fn.getcwd()
           end,
         })
