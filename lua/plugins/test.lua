8e8cd8a89d46b7010d9ba3da27dfc6e2601c24e8
brandon-kyle-bailey <brandon.bailey@produce8.com>
2024-07-12 16:57:24 -0300

    fix test plugin to support intergration and unit tests

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,26 +1,15 @@
 return {
   {
     "nvim-neotest/neotest",
-    dependencies = { "haydenmeade/neotest-jest" },
+    dependencies = {
+      "haydenmeade/neotest-jest",
+    },
     opts = function(_, opts)
       table.insert(
         opts.adapters,
         require("neotest-jest")({
-          jestCommand = "npx jest",
-          jestConfigFile = function(file)
-            if string.find(file, "/packages/") then
-              return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
-            end
-
-            return vim.fn.getcwd() .. "/jest.config.ts"
-          end,
+          jestCommand = "node node_modules/.bin/jest",
           env = { CI = true },
-          cwd = function(file)
-            if string.find(file, "/packages/") then
-              return string.match(file, "(.-/[^/]+/)src")
-            end
-            return vim.fn.getcwd()
-          end,
         })
       )
     end,
