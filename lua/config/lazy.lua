5d87c7e76dca763748b0488433a3f16510980374
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-09-29 03:04:27 -0300

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,7 +45,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lsp.neoconf" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
+    -- { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.util.dot" },
