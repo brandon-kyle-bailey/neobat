671981c5ecd31f58fb208ef17d0588dc31f36192
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2024-10-06 12:30:50 -0300

    add c,go,java & rust support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.go" },
