5c81c3a2e8d17a23717f16930f2e4aa15f83e133
brandon-kyle-bailey <brandon.bailey@produce8.com>
2025-01-14 15:55:33 -0400

    cleanup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -103,7 +103,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.util.chezmoi" },
     { import = "lazyvim.plugins.extras.util.dot" },
-    { import = "lazyvim.plugins.extras.util.gitui" },
+    -- { import = "lazyvim.plugins.extras.util.gitui" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.octo" },
     { import = "lazyvim.plugins.extras.util.project" },
