7c598fc71bf69f383cb9e31217105d2f4d07dbf4
brandon-kyle-bailey <brandon.bailey@produce8.com>
2025-01-14 17:10:46 -0400

    cleanup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -94,7 +94,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
     { import = "lazyvim.plugins.extras.ui.dashboard-nvim" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
+    -- { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.ui.indent-blankline" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
