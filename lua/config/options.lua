8bb4a55acc074f16056a092e56299140c365737f
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2025-04-01 01:03:18 -0300

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = true -- Enable line wrapping
+vim.opt.spell = true
+vim.opt.spelllang = { "en" } -- Use English for spell checking
+vim.g.snacks_animate = false
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/tokyonight.nvim",
+  style = "night",
+  opts = {
+    transparent = true,
+    styles = { sidebars = "transparent", floats = "transparent" },
+  },
+}
