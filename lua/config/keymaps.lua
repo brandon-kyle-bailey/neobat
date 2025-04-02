009bf8ff236c5cb7beb2100267d817244900defa
brandon-kyle-bailey <brandon.bailey@produce8.com>
2025-04-02 16:57:41 -0300

    add path copying for buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,5 +5,17 @@
 -- move 1/2 screen up/down and center vertically
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set("n", "j", "jzz")
 vim.keymap.set("n", "k", "kzz")
+
+-- buffer path copying to clipboard
+vim.keymap.set("n", "<leader>cp", function()
+  vim.fn.setreg("+", vim.fn.expand("%"))
+  print("Copied: " .. vim.fn.expand("%"))
+end, { desc = "Copy the RELATIVE File path of the in-focus buffer" })
+
+vim.keymap.set("n", "<leader>cP", function()
+  vim.fn.setreg("+", vim.fn.expand("%:p"))
+  print("Copied: " .. vim.fn.expand("%:p"))
+end, { desc = "Copy the ABSOLUTE file path of the in-focus buffer" })
