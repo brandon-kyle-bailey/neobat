788bafce23ed42b4d157c1cff8a962c935e56421
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2025-06-08 21:31:59 -0300

    add rest(kulala) plugin

---
+++ lua/plugins/rest.lua
@@ -0,0 +1,12 @@
+return {
+  "mistweaverco/kulala.nvim",
+  keys = {
+    { "<leader>Rb", desc = "Open scratchpad" },
+  },
+  ft = { "http", "rest" },
+  opts = {
+    global_keymaps = true,
+    global_keymaps_prefix = "<leader>R",
+    kulala_keymaps_prefix = "",
+  },
+}
